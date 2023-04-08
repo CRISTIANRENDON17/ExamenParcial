7d2675cf0232fd105b2518602d7899e3458e4f2d
Cristian Rendon <cristianrendon1704@gmail.com>
2023-04-08 11:47:25 -0500

    Tercer commit, add database and icons and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Navbar from './Modules/Navbar/Navbar.jsx';
 import Style from './App.module.css';
 import './styles/index.css';
 
 function App() {
   return (
-    <div className={Style.App}>
+    <div className={Style.container}>
+      <div className={Style.Navbar}>
+        <Navbar></Navbar>
+        <hr />
+      </div>
 
     </div>
   )
---
+++ src/Modules/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import Style from './Navbar.module.css';
+import {AiOutlineShoppingCart} from '@react-icons/all-files/ai/AiOutlineShoppingCart';
+
+const Navbar = () => {
+	return <div className={Style.container}>
+        <h1 className={Style.titulo}>Taller 1</h1>
+        <button className={Style.button}><AiOutlineShoppingCart/> 0 Products </button>
+    </div>;
+};
+
+export default Navbar;
